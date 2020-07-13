5c63a687f50a8d40267dc1f5d824128dc4032427
JordyAaldering <aalderingjordy@gmail.com>
2020-07-13 23:04:45 +0200

    Cleanup

--- Luci-Engine/src/Luci/Renderer/Buffer.h
+++ Luci-Engine/src/Luci/Renderer/Buffer.h
@@ -4,26 +4,24 @@ namespace Luci {
 
 	class VertexBuffer {
 	public:
-		virtual ~VertexBuffer() {}
+		static VertexBuffer* Create(float* vertices, size_t size);
+		virtual ~VertexBuffer() = default;
 
 		virtual void Bind() const = 0;
 		virtual void UnBind() const = 0;
 
 		virtual uint32_t GetCount() const = 0;
-
-		static VertexBuffer* Create(float* vertices, size_t size);
 	};
 
 	class IndexBuffer {
 	public:
-		virtual ~IndexBuffer() {}
+		static IndexBuffer* Create(uint32_t* indices, size_t size);
+		virtual ~IndexBuffer() = default;
 
 		virtual void Bind() const = 0;
 		virtual void UnBind() const = 0;
 
 		virtual uint32_t GetCount() const = 0;
-
-		static IndexBuffer* Create(uint32_t* indices, size_t size);
 	};
 
 }
