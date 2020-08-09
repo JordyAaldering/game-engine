47762ab0f49603b474368b36426a7a0e9530801f
JordyAaldering <aalderingjordy@gmail.com>
2020-08-09 12:25:58 +0200

    Fix input

--- Luci-Engine/src/Luci/Core/Input.h
+++ Luci-Engine/src/Luci/Core/Input.h
@@ -6,30 +6,14 @@ namespace Luci {
 
 	class Input {
 	public:
-		static Scope<Input> Create();
+		// Key input
+		static bool IsKeyPressed(int keyCode);
 
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
-
-		inline static bool IsKeyPressed(int keyCode) { return s_Instance->IsKeyPressedImpl(keyCode); }
-
-		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		inline static std::pair<float, float> GetMousePos() { return s_Instance->GetMousePosImpl(); }
-		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-
-	protected:
-		Input() = default;
-
-		virtual bool IsKeyPressedImpl(int keyCode) = 0;
-
-		virtual bool IsMouseButtonPressedImpl(int button) = 0;
-		virtual std::pair<float, float> GetMousePosImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-
-	private:
-		static Scope<Input> s_Instance;
+		// Mouse input
+		static bool IsMouseButtonPressed(int button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 
 }
--- Luci-Engine/src/Platform/Windows/WindowsInput.cpp
+++ Luci-Engine/src/Platform/Windows/WindowsInput.cpp
@@ -1,37 +1,38 @@
 #include "lucipch.h"
-#include "WindowsInput.h"
+
+#include "Luci/Core/Input.h"
 #include "Luci/Core/Application.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Luci {
 
-	bool WindowsInput::IsKeyPressedImpl(int keyCode) {
+	bool Input::IsKeyPressed(int keyCode) {
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		int state = glfwGetKey(window, keyCode);
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(int button) {
+	bool Input::IsMouseButtonPressed(int button) {
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		int state = glfwGetMouseButton(window, button);
 		return state == GLFW_PRESS;
 	}
 
-	std::pair<float, float> WindowsInput::GetMousePosImpl() {
+	std::pair<float, float> Input::GetMousePosition() {
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xPos, yPos;
 		glfwGetCursorPos(window, &xPos, &yPos);
 		return { (float)xPos, (float)yPos };
 	}
 
-	float WindowsInput::GetMouseXImpl() {
-		auto [xPos, _] = GetMousePosImpl();
+	float Input::GetMouseX() {
+		auto [xPos, _] = GetMousePosition();
 		return xPos;
 	}
 
-	float WindowsInput::GetMouseYImpl() {
-		auto [_, yPos] = GetMousePosImpl();
+	float Input::GetMouseY() {
+		auto [_, yPos] = GetMousePosition();
 		return yPos;
 	}
 
