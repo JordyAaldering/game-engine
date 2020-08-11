17c205250c0fbfd5838f71b2c159bc427c74b141
JordyAaldering <aalderingjordy@gmail.com>
2020-08-11 17:09:09 +0200

    Update Core.h

--- Luci-Engine/src/Luci/Core/Core.h
+++ Luci-Engine/src/Luci/Core/Core.h
@@ -65,7 +65,7 @@
  * Utilities 
  */
 
-#define LUCI_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define LUCI_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace Luci {
 
